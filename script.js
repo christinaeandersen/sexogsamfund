b5e10aa7982985e8a2c686fd8510f1b545accd9e
maria crone hvidberg <marife44@stud.kea.dk>
2018-11-16 14:16:00 +0100

    start skærm og scenarie 1 færdig (mangler keyframes)

--- script.js
+++ script.js
@@ -38,10 +38,62 @@ function showScenarie1() {
 }
 
 function animationSenarie1() {
-    document.querySelector("#telefon").classList.add("shake");
+    document.querySelector("#telefon_tissemand").classList.add("shake");
     document.querySelector("#telefon").classList.remove("show");
-    document.querySelector("#")
+    document.querySelector("#telefon_tissemand").classList.add("show");
     document.querySelector("#besked_tone").play();
     document.querySelector("#vibration_mobil").play();
 
+    showOptions1();
+
+}
+
+function showOptions1() {
+    console.log("showOptions1");
+    document.querySelector("#dilemma1").classList.add("show");
+    document.querySelector("#valg1").classList.add("show");
+    document.querySelector("#valg2").classList.add("show");
+    document.querySelector("#valg3").classList.add("show");
+
+    document.querySelector("#valg1").addEventListener("click", showResult1_1);
+
+    document.querySelector("#valg2").addEventListener("click", showResult1_2);
+
+    document.querySelector("#valg3").addEventListener("click", showResult1_3);
+}
+
+function showResult1_1() {
+    document.querySelector("#scenarie1").classList.remove("show");
+    document.querySelector("#telefon_tissemand").classList.remove("shake");
+    document.querySelector("#telefon_tissemand").classList.remove("show");
+    document.querySelector("#dilemma1").classList.remove("show");
+    document.querySelector("#valg1").classList.remove("show");
+    document.querySelector("#valg2").classList.remove("show");
+    document.querySelector("#valg3").classList.remove("show");
+
+    document.querySelector("#result2").classList.add("show");
+}
+
+function showResult1_2() {
+    document.querySelector("#scenarie1").classList.remove("show");
+    document.querySelector("#telefon_tissemand").classList.remove("shake");
+    document.querySelector("#telefon_tissemand").classList.remove("show");
+    document.querySelector("#dilemma1").classList.remove("show");
+    document.querySelector("#valg1").classList.remove("show");
+    document.querySelector("#valg2").classList.remove("show");
+    document.querySelector("#valg3").classList.remove("show");
+
+    document.querySelector("#result1").classList.add("show");
+}
+
+function showResult1_3() {
+    document.querySelector("#scenarie1").classList.remove("show");
+    document.querySelector("#telefon_tissemand").classList.remove("shake");
+    document.querySelector("#telefon_tissemand").classList.remove("show");
+    document.querySelector("#dilemma1").classList.remove("show");
+    document.querySelector("#valg1").classList.remove("show");
+    document.querySelector("#valg2").classList.remove("show");
+    document.querySelector("#valg3").classList.remove("show");
+
+    document.querySelector("#result3").classList.add("show");
 }
